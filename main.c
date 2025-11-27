e7e88c223a4b007c479d8556ec3523ea2aa7bda8
saleemsaleem51296 <saleemsaleem51296@gmail.com>
2025-11-26 22:19:15 -0800

    even or odd program

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    
+    if(n % 2==0)
+      printf("%d is even number:");
+      else
+      printf("%d is odd number:");
+      
+}
